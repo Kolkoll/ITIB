ec58b86172d3e0d16cf0ab40a5d86105f508e8ea
Kolkoll <34005244+Kolkoll@users.noreply.github.com>
2018-01-11 12:56:44 +0300

    Add files via upload

---
+++ DZ/Back propagation of error.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ
+{
+    class Back_propagation_of_error
+    {
+        double[] weighting_coefficients_of_hidden_layer = { 0f, 0f };
+        double[,] weighting_coefficients_of_out_layer = { { 0f, 0f }, { 0f, 0f }, { 0f, 0f }};
+
+        public double counting_net_exit(string layer, double[] X, int start)
+        {
+            double net_exit = 0f;
+            double sum = 0f;
+            if (layer == "Скрытый")
+            {
+                for (int index = 0; index < weighting_coefficients_of_hidden_layer.Length; index++)
+                    sum += weighting_coefficients_of_hidden_layer[index] * X[index];
+                net_exit = sum;
+                return net_exit;
+            }
+            if (layer == "Выходной")
+            {
+                net_exit += weighting_coefficients_of_out_layer[start, 0];
+                net_exit += weighting_coefficients_of_out_layer[start, 1] * X[0];
+                return net_exit;
+            }
+            return 0f;
+        }
+
+        public double[] function_of_activation(double[] net)
+        {
+            double[] out_exit = new double[net.Length];
+            for (int index = 0; index < net.Length; index++)
+            {
+                out_exit[index] = 1 / (1 + Math.Exp(-net[index]));
+           //     Console.Write(" {0}", out_exit[index]);
+            }
+            return out_exit;
+        }
+
+        public double[] counting_delta_error(double[] Y_exit, double[] T)
+        {
+            double[] delta = new double[Y_exit.Length];
+            for (int index = 0; index < Y_exit.Length; index++)
+                delta[index] = Y_exit[index] - T[index];
+            return delta;
+        }
+
+        public double[] counting_mistake_out_layer(double[] Y_exit, double[] T, double[] net)
+        {
+            double[] mistake = new double[Y_exit.Length];
+            for (int index = 0; index < Y_exit.Length; index++)
+                mistake[index] = (T[index] - Y_exit[index]) * (1 / (1 + Math.Exp(-net[index])) *
+                    (1 - (1 / (1 + Math.Exp(-net[index])))));
+            return mistake;
+        }
+
+        public double[] counting_mistake_hidden_layer(double[] net, double[] mistake_out_layer)
+        {
+            double[] mistake = new double[2];
+            double sum = 0f;
+            for (int indexx = 0; indexx < mistake.Length; indexx++)
+            {
+                for (int index = 0; index < weighting_coefficients_of_out_layer.Length / 2; index++)
+                    sum += mistake_out_layer[index] * weighting_coefficients_of_out_layer[index, indexx];
+                mistake[indexx] = (1 / (1 + Math.Exp(-net[0])) * (1 - (1 / (1 + Math.Exp(-net[0]))))) * sum;
+            }
+            return mistake;
+        }
+
+        public void correction_of_weighting_coefficients(double[] X, double[] delta, string layer)
+        {
+            if (layer == "Выходной")
+            {
+          //      Console.WriteLine("Веса выходного слоя:");
+                double[,] delta_w = new double[weighting_coefficients_of_out_layer.Length / 2,
+                    weighting_coefficients_of_out_layer.Length / 3];
+                for (int i1_index = 0; i1_index < weighting_coefficients_of_out_layer.Length / 2; i1_index++)
+                {
+                    for (int i2_index = 0; i2_index < weighting_coefficients_of_out_layer.Length / 3; i2_index++)
+                    {
+                        delta_w[i1_index, i2_index] = X[i2_index] * delta[i1_index];
+                        weighting_coefficients_of_out_layer[i1_index, i2_index] += delta_w[i1_index, i2_index];
+              //          Console.Write(", {0}", weighting_coefficients_of_out_layer[i1_index, i2_index]);
+                    }
+                //    Console.WriteLine();
+                }
+            }
+            if (layer == "Скрытый")
+            {
+         //       Console.WriteLine("Веса скрытого слоя:");
+                double[] delta_w_c = new double[weighting_coefficients_of_hidden_layer.Length];
+                for (int index = 0; index < delta_w_c.Length; index++)
+                {
+                    delta_w_c[index] = X[index] * delta[index];
+                    weighting_coefficients_of_hidden_layer[index] += delta_w_c[index];
+             //       Console.Write(", {0}", weighting_coefficients_of_hidden_layer[index]);
+                }
+            }
+
+        }
+    }
+}
---
+++ DZ/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Обучение МНС методом обратного распространения ошибки");
+                double[] x = { 1, -1 };
+                double[] t = {-1, 2, 2 };
+                Back_propagation_of_error obj = new Back_propagation_of_error();
+                int quit = 0;
+                while(true)
+                {
+                    Console.WriteLine();
+                    double[] net1 = new double[1];
+                    net1[0] = obj.counting_net_exit("Скрытый", x, 0);
+                    double[] out1 = new double[1];
+//                    Console.WriteLine("Скрытый слой нейронов:");
+                    out1 = obj.function_of_activation(net1);
+ //                   Console.WriteLine();
+                    double[] x2 = new double[1];
+                    x2[0] = out1[0];
+                    double[] net2 = new double[3];
+                    for (int index = 0; index < net2.Length; index++)
+                        net2[index] = obj.counting_net_exit("Выходной", x2, index);
+ //                   Console.WriteLine();
+                    double[] out2 = new double[3];
+                    Console.WriteLine("Выходной слой нейронов:");
+                    out2 = obj.function_of_activation(net2);
+                    for (int index = 0; index < out2.Length; index++)
+                        Console.Write(" {0:f2}", out2[index]);
+                    Console.WriteLine();
+  //                  for (int i = 0; i < out2.Length; i++)
+  //                      Console.WriteLine(out2[i]);
+                    double[] delta = obj.counting_delta_error(out2, t);
+    //                for (int index = 0; index < delta.Length; index++)
+     //                   Console.Write(" {0}", delta[index]);
+      //              Console.WriteLine();
+                    quit++;
+     //               for (int i = 0; i < delta.Length; i++)
+      //                  Console.Write(", {0}", delta[i]);
+                    Console.WriteLine("Ошибки выходного слоя:");
+                    double[] mistake = obj.counting_mistake_out_layer(out2, t, net2);
+                    for (int index = 0; index < mistake.Length; index++)
+                        Console.Write(" {0:f2}", mistake[index]);
+                    Console.WriteLine();
+     //               for (int i = 0; i < mistake.Length; i++)
+     //                   Console.WriteLine(mistake[i]);
+                    Console.WriteLine("Ошибки скрытого слоя:");
+                    double[] mistake2 = obj.counting_mistake_hidden_layer(net1, mistake);
+                    for (int index = 0; index < mistake2.Length; index++)
+                        Console.Write(" {0:f2}", mistake2[index]);
+                    Console.WriteLine();
+     //               for (int i = 0; i < mistake2.Length; i++)
+     //                   Console.WriteLine(mistake2[i]);
+                    double[] x_var = {1f, x2[0]};
+                    obj.correction_of_weighting_coefficients(x_var, mistake, "Выходной");
+                    obj.correction_of_weighting_coefficients(x, mistake2, "Скрытый");
+                    Console.WriteLine();
+                    if (quit == 2)
+                        break;
+                }
+                Console.WriteLine("Для продолжения нажмите ENTER, для выхода - любую другую клавишу");
+                keyInfo = Console.ReadKey();
+            } while (keyInfo.Key == ConsoleKey.Enter);
+        }
+    }
+}
